9bc9b875966962b06329e08033cc140a5911427c
Débora R. Romagnolo <30395276+dederomagnolo@users.noreply.github.com>
2019-02-06 16:19:12 -0200

    Add files via upload

---
+++ BeThere_V1.ino
@@ -0,0 +1,107 @@
+#include "ThingSpeak.h"
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+EthernetClient client;
+
+//Atribuição dos pinos
+#define sensor A0
+#define bomba 3
+#define ledVerde 8
+#define ledAmarelo 9
+#define ledVermelho 10
+#define luz A1
+
+//variáveis para gravação
+int umidade;
+int luminosidade;
+String nivelUmidade;
+
+//Campos do ThingSpeak
+unsigned long myChannelNumber = 695672;
+const char * myWriteAPIKey = "ZY113X3ZSZG96YC8";
+
+void setup() {
+  
+  Serial.begin(9600);
+  Ethernet.begin(mac);
+  ThingSpeak.begin(client);
+  
+  //Entradas e saída
+  pinMode(sensor, INPUT); //sensor de umidade
+  pinMode(luz, INPUT); //sensor de luminosidade (conectada ao divisor de tensão com fotoresistor)
+  pinMode(bomba, OUTPUT); //acionamento do relé
+
+  //Leds indicadores
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledAmarelo, OUTPUT);
+  pinMode(ledVermelho, OUTPUT);
+
+  digitalWrite(bomba, HIGH);
+}
+
+void loop() {
+
+  //leitura da umidade
+  umidade = analogRead(sensor);
+
+  //classificação do solo
+  if (umidade >= 200 && umidade <=500){
+    
+    nivelUmidade = "Úmido";
+
+    //Acende led verde
+    digitalWrite(ledVerde, HIGH);
+    digitalWrite(ledVermelho, LOW);
+    digitalWrite(ledAmarelo, LOW);
+    digitalWrite(bomba, HIGH);
+  } 
+  
+  else if (umidade > 500 && umidade < 850){
+    
+    nivelUmidade = "Quase Seco";
+
+    //Acende led amarelo
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledVermelho, LOW);
+    digitalWrite(ledAmarelo, HIGH); 
+    digitalWrite(bomba, HIGH);
+  }
+  
+  else if(umidade >= 850 && umidade <= 1024)
+  { 
+    
+    nivelUmidade = "Seco";
+
+    //Acende led vermelho
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledVermelho, HIGH);
+    digitalWrite(ledAmarelo, LOW);
+    digitalWrite(bomba, LOW);
+  }
+
+  //ajustar limites para exibir o valor de 0 - 100 (seco-umido/sem luz-com luz)
+  umidade = map(umidade, 0, 1024, 100, 0); 
+  
+  //leitura da ponta analógica do divisor de tensão com foto resistor
+  luminosidade = analogRead(luz);
+  luminosidade = map(luminosidade, 0, 1024, 0, 100);
+
+  //plot no serial monitor
+  Serial.print("\numidade: ");
+  Serial.print(umidade);
+  Serial.print("%");
+  Serial.print("\nluminosidade: ");
+  Serial.print(luminosidade);
+  Serial.print("%");
+  Serial.print("\n");
+  Serial.print(nivelUmidade);
+
+  //escrever no field 1 - ThingSpeak
+  ThingSpeak.writeField(myChannelNumber, 1, umidade, myWriteAPIKey);
+  
+  //delay de 20
+  delay(2000); // ThingSpeak precisa de pelo menos 15s de intervalo
+
+}
